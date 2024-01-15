de2b229f983e9f61cd3887b571ba2ef457044534
Fernando Fraga <fernando_fraga@dell.com>
2024-01-15 15:02:21 -0300

    Fixing imports

--- src/main/java/com/tenniscourts/schedules/Schedule.java
+++ src/main/java/com/tenniscourts/schedules/Schedule.java
@@ -14,13 +14,10 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
